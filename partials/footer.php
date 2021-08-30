f219bd13cd890eac43de07cf0a23341cb00e0676
Bart <bart.rybak@hotmail.com>
2021-08-30 21:17:58 +0100

    blog bootstrap template added

--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,22 @@
-<?php
+    </div><!-- /.row -->
+
+</div><!-- /.container -->
+<footer class="blog-footer">
+        <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+        <p>
+            <a href="#">Back to top</a>
+        </p>
+    </footer>
+    
 
 
 
-?>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <script src="/public/js/bootstrap.js"></script>
+</body>
+
+</html>
--- partials/header.php
+++ partials/header.php
@@ -1,5 +1,47 @@
 <?php
 
-
+include('../includes/config.php');
+include('../includes/db.php');
 
 ?>
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Blog</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/blog.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div class="blog-masthead">
+        <div class="container">
+            <nav class="blog-nav">
+                <a class="blog-nav-item active" href="#">Home</a>
+                <a class="blog-nav-item" href="#">New features</a>
+                <a class="blog-nav-item" href="#">Press</a>
+                <a class="blog-nav-item" href="#">New hires</a>
+                <a class="blog-nav-item" href="#">About</a>
+            </nav>
+        </div>
+    </div>
+
+    <div class="container">
+
+
+
+        <div class="row">
+
+            <div class="col-sm-8 blog-main">
