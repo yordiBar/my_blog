cfd3628dccae3efed563fd9d38411bcf4acc77f2
Bart <bart.rybak@hotmail.com>
2021-09-06 23:00:59 +0100

    search posts working

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -3,7 +3,17 @@
 
 
 ?>
+
 <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+    <div class="sidebar-module">
+    <h4>Search</h4>
+    <hr>
+        <form method="get" action="results.php" class="form-inline">
+            <div class="form-group">
+                <input type="text" name="search" class="form-control" id="exampleInputName2" placeholder="Search...">
+            </div>
+        </form>
+    </div>
                 <div class="sidebar-module sidebar-module-inset">
                     <h4>About</h4>
                     <p><?php echo $about_text ?></p>
